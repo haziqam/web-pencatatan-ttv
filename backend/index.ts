ac724866dd64745c74dbd74e2d812cb1db9aed6c
haziqam <103514359+haziqam@users.noreply.github.com>
2024-06-22 18:36:34 +0700

    feat: cache interface

--- backend/index.ts
+++ backend/index.ts
@@ -1,17 +1,15 @@
-import express, { Request, Response } from 'express';
-import dotenv from 'dotenv';
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.EXPRESS_PORT || 3000;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, TypeScript with Express!');
+app.get("/", (req: Request, res: Response) => {
+    res.send("Hello, TypeScript with Express!");
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+    console.log(`Server is running on http://localhost:${port}`);
 });
-
-
