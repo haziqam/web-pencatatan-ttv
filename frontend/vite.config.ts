443893f3ad5e72db1bc2f1a83a91670d57ae0441
haziqam <103514359+haziqam@users.noreply.github.com>
2024-06-22 22:07:32 +0700

    chore: update dotenv.config() in vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,22 +4,16 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import dotenv from 'dotenv';
+import dotenv from 'dotenv'
 
-dotenv.config({
-  path: "../.env"
-});
+dotenv.config()
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueJsx(), vueDevTools()],
   server: {
     host: '0.0.0.0',
-    port: parseInt(process.env.VUE_PORT ?? '5173', 10),
+    port: parseInt(process.env.VUE_PORT ?? '5173', 10)
   },
   resolve: {
     alias: {
