b9c6501ad12e7416b5f0c131b7d8f7318b199c07
haziqam <103514359+haziqam@users.noreply.github.com>
2024-06-24 23:11:36 +0700

    feat: add primevue library

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,11 +3,18 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import { router } from "./router";
 import piniaPluginPersistedState from "pinia-plugin-persistedstate";
+import PrimeVue from "primevue/config";
+import Aura from "@primevue/themes/aura";
 
 const app = createApp(App);
 const pinia = createPinia();
 pinia.use(piniaPluginPersistedState);
 app.use(pinia);
 app.use(router);
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+  },
+});
 
 app.mount("#app");
